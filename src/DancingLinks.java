45a8ac633d3ce59725504554471a45c19c8a094c
gl7fc <rsu.merrypink@gmail.com>
2025-12-09 14:08:59 +0900

    エラー解消

--- src/DancingLinks.java
+++ src/DancingLinks.java
@@ -4,35 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-// 制約行列 → DLXで解く
 public class DancingLinks {
 
-    // Dancing Links のノード
-    // ノード同士のリンクを切ったりつなげたり
     class Node {
-        Node L, R, U, D; // すべてのノードには左右上下にリンクするノードが存在
-        int Row; // 行番号
-        ColumnNode Col; // すべてのノードには対応する列ヘッダが存在
+        Node L, R, U, D;
+        int Row;
+        ColumnNode Col;
 
-        // コンストラクタ
-        // 左右上下, 自分自身にリンクするよう初期化
         public Node() {
             L = R = U = D = this;
         }
 
-        // コンストラクタ
-        // ノードと指定の列ヘッダを対応させる
         public Node(ColumnNode c, int r) {
             this();
             Col = c;
             Row = r;
         }
 
-        // ノードn1をノードthisの下にリンク
         Node hookDown(Node n1) {
-            // n1がthisと同じ列じゃないときエラー
             assert (this.Col == n1.Col);
-
             n1.D = this.D;
             n1.D.U = n1;
             n1.U = this;
@@ -40,7 +30,6 @@ public class DancingLinks {
             return n1;
         }
 
-        // ノードn1をノードthisの右にリンク
         Node hookRight(Node n1) {
             n1.R = this.R;
             n1.R.L = n1;
@@ -49,63 +38,51 @@ public class DancingLinks {
             return n1;
         }
 
-        // ノードthisの左右のリンクを外す
         void unlinkLR() {
             this.L.R = this.R;
             this.R.L = this.L;
         }
 
-        // thisからリンクを外した左右のノードをthisにリンク
         void relinkLR() {
             this.L.R = this.R.L = this;
         }
 
-        // ノードthisの上下のリンクを外す
         void unlinkUD() {
             this.U.D = this.D;
             this.D.U = this.U;
         }
 
-        // thisからリンクを外した上下のノードをthisにリンク
         void relinkUD() {
             this.U.D = this.D.U = this;
         }
     }
 
-    // 列ヘッダを表すノード
     class ColumnNode extends Node {
-        int size; // 列のサイズ
-        int covered; // 列にあるノードのうち削除されたノード数
-        int constraint; // 列の制約数
-        String name; // 列の名前
+        int size;
+        int covered;
+        int constraint;
+        String name;
 
-        // コンストラクタ
-        // 列名をつける
         public ColumnNode(String n, int i) {
             super();
             size = 0;
             covered = 0;
             constraint = i;
             name = n;
-            Col = this; // 列ヘッダは自分
+            Col = this;
         }
 
-        // 列jを削除する
         void cover() {
-            unlinkLR(); // まず列ヘッダを削除
-            // 列にあるノードについて
+            unlinkLR();
             for (Node i = this.D; i != this; i = i.D) {
-                // "1"のノードがある行を削除する
                 for (Node j = i.R; j != i; j = j.R) {
                     j.unlinkUD();
                     j.Col.size--;
                 }
             }
-            header.size--; // 列ヘッダの数を1減らす
+            header.size--;
         }
 
-        // 列jを復元する
-        // cover() と逆順の処理
         void uncover() {
             for (Node i = this.U; i != this; i = i.U) {
                 for (Node j = i.L; j != i; j = j.L) {
@@ -114,13 +91,13 @@ public class DancingLinks {
                 }
             }
             relinkLR();
-            header.size++; // 列ヘッダの数を1増やす
+            header.size++;
         }
     }
 
-    private ColumnNode header; // 列ヘッダをまとめるノード
-    private List<Node> answer; // 解答を保存するリスト
-    private List<Node> allSolutions = new ArrayList<>(); // 最終結果を保存するリスト
+    private ColumnNode header;
+    private List<Node> answer;
+    private List<List<Node>> allSolutions = new ArrayList<>(); // 全解を保存
 
     // Algorithm X
     // k: 再帰の深さ
@@ -145,42 +122,29 @@ public class DancingLinks {
         c.cover();
 
         for (Node r = c.D; r != c; r = r.D) {
-                answer.add(r);
+            answer.add(r);
 
-                // 3.b. X[r][j] = 1 であるようなすべての列jについて
-                for (Node j = r.R; j != r; j = j.R) {
-
-                    // 3.b.i. 列jのカウンタを1増やす
-                    j.Col.covered++;
-
-                    // 3.b.ii. 列jのカウンタが地雷の制約数を満たしている場合,
-                    if (j.Col.covered >= j.Col.constraint) {
-                        j.Col.cover();
-                    }
-                }
-
-                // 3.c. 行と列が削除された行列について, このアルゴリズムをもう一度適用
-                knuthsAlgorithmX(k + 1);
-
-                // 解答のノードリストから最後のノードを削除, そのノードをrに代入
-                r = answer.remove(answer.size() - 1);
-
-                // 最後に選んだ解を次の繰り返しで削除するようcに代入
-                c = r.Col;
-
-                // 最後に削除した行から辿れる列を復元
-                for (Node j = r.L; j != r; j = j.L) {
-                    if (j.Col.covered >= j.Col.constraint) {
-                        j.Col.uncover();
-                    }
-                    j.Col.covered--;
+            for (Node j = r.R; j != r; j = j.R) {
+                j.Col.covered++;
+                if (j.Col.covered >= j.Col.constraint) {
+                    j.Col.cover();
                 }
             }
+
+            knuthsAlgorithmX(k + 1);
+
+            r = answer.remove(answer.size() - 1);
+            c = r.Col;
+
+            for (Node j = r.L; j != r; j = j.L) {
+                j.Col.uncover();
+                j.Col.covered--;
+            }
+        }
 
         c.uncover();
     }
 
-    // 属しているノード数の少ない列を探索
     private ColumnNode selectColumnNode() {
         int min = Integer.MAX_VALUE;
         ColumnNode ret = null;
@@ -194,79 +158,52 @@ public class DancingLinks {
         return ret;
     }
 
-    // 引数に取られた制約行列をもとにダンシングリンクを構成する
-    // constraint は各列の地雷数の制約を入れた配列
-    // 制約行列の1の値を持っている部分のみダンシングリンク化するという疎行列を形成する
     private ColumnNode makeDLXBoard(int[][] grid, int[] constraint) {
-        final int COLS = grid[0].length; // ECP行列の列数
-        final int ROWS = grid.length; // ECP行列の行数
+        final int COLS = grid[0].length;
+        final int ROWS = grid.length;
 
-        // カラムノードの現在リンクが済んでいる最新の部分を
-        // "header"という名前を付けてインスタンス化
         ColumnNode headerNode = new ColumnNode("header", 0);
-        // カラムノードの集合
         ArrayList<ColumnNode> columnNodes = new ArrayList<ColumnNode>();
 
-        // 全ての列に関してカラムノードをインスタンス化する
-        // 名前は何列目かを表す
         for (int i = 0; i < COLS; i++) {
-            // i列目のカラムノードをインスタンス化
             ColumnNode n = new ColumnNode(Integer.toString(i), constraint[i]);
-            // カラムノードのリストに追加
             columnNodes.add(n);
-            // headerとi-1列目の間にi列目のカラムノードをリンクさせる
-            // リンクさせたnをheaderノードとする
             headerNode = (ColumnNode) headerNode.hookRight(n);
         }
 
-        // headerノードを最後の列に持ってくる
         headerNode = (ColumnNode) headerNode.R;
 
         for (int i = 0; i < ROWS; i++) {
             Node prev = null;
             for (int j = 0; j < COLS; j++) {
-                // ECP行列のi行j列目の値が1の場合...条件式1
                 if (grid[i][j] == 1) {
-                    // カラムノードのリストからj列目のノードを取り出しcolに代入
                     ColumnNode col = columnNodes.get(j);
-                    // j列目にダンシングノードを作る
                     Node newNode = new Node(col, i);
-                    // 条件式1に初めて入る場合
                     if (prev == null) {
-                        // newNodeをprevに代入
                         prev = newNode;
                     }
-
-                    // newNode0行j列目に代入
-                    // 左右上下とリンクする
                     col.U.hookDown(newNode);
                     prev = prev.hookRight(newNode);
-                    // j列目に含まれる1の数をインクリメント
                     col.size++;
                 }
             }
         }
 
-        // 最後の列のノードのsizeはCOLS
         headerNode.size = COLS;
-        // 最後の列のカラムノードを返す
         return headerNode;
     }
 
-    // コンストラクタ
     public DancingLinks(int[][] grid, int[] constraint) {
         header = makeDLXBoard(grid, constraint);
     }
 
-    // 数独解読の実行
     public void runSolver() {
-        // 解答用のリストをインスタンス化
         answer = new LinkedList<Node>();
         allSolutions.clear();
         knuthsAlgorithmX(0);
     }
 
-    // 全解で共通するセル(確定するセル)のみを返す
+    // 全解で共通するセルのみを返す
     public Set<Integer> getDeducedCells() {
         if (allSolutions.isEmpty()) {
             return new HashSet<>();
@@ -310,14 +247,13 @@ public class DancingLinks {
         return ans;
     }
 
-    // 解を表示
     public void showAnswer() {
         System.out.println("--- Found " + allSolutions.size() + " solution(s) ---");
         for (int s = 0; s < allSolutions.size(); s++) {
             System.out.print("Solution " + (s + 1) + ": ");
             for (Node n : allSolutions.get(s)) {
                 System.out.print(n.Row + " ");
-    }
+            }
             System.out.println();
         }
     }
